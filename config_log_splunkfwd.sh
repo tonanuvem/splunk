f5fada362c8bff7170df08ae63d03093a3561e3a
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-09-16 22:40:05 -0300

    Update config_log_splunkfwd.sh with documentation links
    Added installation and configuration links for Splunk Universal Forwarder.

--- config_log_splunkfwd.sh
+++ config_log_splunkfwd.sh
@@ -1,4 +1,5 @@
 # INSTALL
+# https://help.splunk.com/en/splunk-cloud-platform/forward-and-process-data/universal-forwarder-manual/9.1/install-the-universal-forwarder/install-a-nix-universal-forwarder#bfa92018_7238_476c_8351_2dd1ee65ef8c--en__Install_the_universal_forwarder_on_Linux
 
 sudo useradd -m splunkfwd
 sudo groupadd splunkfwd
@@ -9,6 +10,7 @@ mkdir $SPLUNK_HOME
 
 cd $SPLUNK_HOME
 
+# DOWNLOAD
 # https://www.splunk.com/en_us/download/universal-forwarder.html
 wget -O splunkforwarder-10.4.3-4174a2deda5d-linux-amd64.tgz "https://download.splunk.com/products/universalforwarder/releases/10.4.3/linux/splunkforwarder-10.4.3-4174a2deda5d-linux-amd64.tgz"
 
@@ -19,6 +21,7 @@ chown -R splunkfwd:splunkfwd $SPLUNK_HOME
 sudo $SPLUNK_HOME/bin/splunk start --accept-license
 
 # CONFIG
+# https://help.splunk.com/en/splunk-cloud-platform/forward-and-process-data/universal-forwarder-manual/10.4/configure-the-universal-forwarder/configure-the-universal-forwarder-using-configuration-files
 
 # Navigate to outputs.conf in $SPLUNK_HOME/etc/system/local/ to locate your Universal Forwarder configuration files.
 # Key configuration files:
