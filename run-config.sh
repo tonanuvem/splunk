98cf9e7ebc95bef3ebca2b2056a30a12fc044331
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-09-18 10:44:23 -0300

    Modify Splunk Enterprise configuration command

--- run-config.sh
+++ run-config.sh
@@ -2,7 +2,7 @@
 sudo bash ~/splunk/config_docker_otel.sh
 
 # 2. Splunk Enterprise — precisa vir antes do instalador da aplicação:
-sudo bash ~/splunk/config_splunk_enterprise.sh
+sudo bash GLIBC_TUNABLES=glibc.pthread.rseq=0 ~/splunk/config_splunk_enterprise.sh
 
 # 3. Log Observer Connect: -  O Log Observer Connect deixa voce pesquisar, dentro do Splunk Observability
 # Cloud, logs que estao num Splunk plataforma (Enterprise ou Cloud). Os logs NAO sao copiados.
